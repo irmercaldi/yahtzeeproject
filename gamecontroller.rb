75e6ef56bd5cdc9cac778785342fc2bc4d0128f4
Stephanie Pinion <stephpinion@gmail.com>
2017-03-31 10:46:24 -0600

    Initial class and turn method

--- gamecontroller.rb
+++ gamecontroller.rb
@@ -1,1 +1,28 @@
-gamecontroller.rb
+require "pry"
+# require "scorecard.rb"
+
+# class Game
+
+#   def roll
+#     # empty array of 5 dice
+#     dice = []
+
+
+
+#     # random 5 numbers between 1 - 6
+
+#   end
+
+#   def turn
+#     #variable for how many turns
+#     #roll up to three times
+
+#     # After roll user choice for dice to keep.
+
+#   end
+
+# end
+
+p (1..100).rand(5) do |x|
+  p "#{x}"
+end
