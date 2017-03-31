a79f6b7a270203c18a9a4a750cdbc99fd967da43
Stephanie Pinion <stephpinion@gmail.com>
2017-03-31 13:37:05 -0600

    dice roll to array

--- gamecontroller.rb
+++ gamecontroller.rb
@@ -1,28 +1,62 @@
 require "pry"
-# require "scorecard.rb"
-
-# class Game
-
-#   def roll
-#     # empty array of 5 dice
-#     dice = []
 
 
+  def roll die
+    # empty array of 5 dice
+    dice = []
+    die.times do
+      dice << rand(1..6)
+    end
+    dice
+  end
 
-#     # random 5 numbers between 1 - 6
+  def turn
+    #variable for how many rolls up to 3
+    times = 0 #how many rolls
+    dice_hash = Hash.new()
 
-#   end
+    if times < 3
+      #show what was rolled
+      p "Here is your roll: " + roll(5).to_s
+      times =+ 1
+    end
 
-#   def turn
-#     #variable for how many turns
-#     #roll up to three times
+    #after each roll user input for which values to keep - 
+    puts "Roll Agian? Yes or No"
 
-#     # After roll user choice for dice to keep.
+    again = gets.chomp.downcase
 
-#   end
+#change to loop
+    if again == "no"
+      # show scorecard from user of what are left options
+      # user input to which box to us (scorecard)
+      # sum of dice
 
-# end
+    elsif again == "yes"
+      p "what dice numbers would you like to keep?"
+      #take die and convert to hash 
+      keep_dice = gets.chomp
+      p keep_dice.split(" ")
+      # reset dice
+      #manydice = (5 - keep_dice.length)
+      roll(5 - keep_dice.length)
+      times =+ 1
+      binding.pry
+    else
+      p "Opps!"
 
-p (1..100).rand(5) do |x|
-  p "#{x}"
-end
+    end
+      
+
+
+    # call roll with how many dice to roll
+ 
+   # save hash 
+       
+
+   #pass hash to scorecard
+
+  end
+
+turn
+
